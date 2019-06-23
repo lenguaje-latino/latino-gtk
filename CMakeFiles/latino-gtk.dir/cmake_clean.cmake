335204d01da370d12d6cba6b0d9d00f262b055df
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2019-06-22 22:23:30 -0500

    primer commit

---
+++ CMakeFiles/latino-gtk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/latino-gtk.dir/src/latino-gtk.c.o"
+  "liblatino-gtk.pdb"
+  "liblatino-gtk.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/latino-gtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
